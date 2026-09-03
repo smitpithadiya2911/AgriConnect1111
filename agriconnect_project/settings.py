58da89a0b1f1e741826ebce12acc965a408da94c
smitpithadiya2911 <gohilkrishna436@gmail.com>
2026-09-04 00:25:41 +0530

    Deploy architecture update: pre-collect staticfiles and streamline vercel.json with single serverless Python build

--- agriconnect_project/settings.py
+++ agriconnect_project/settings.py
@@ -36,6 +36,12 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.vercel.app',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
+
 # Application definition
 
 INSTALLED_APPS = [

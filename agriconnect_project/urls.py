731b43527a537aeadbb18290ea2cb8a1931533bc
smitpithadiya2911 <gohilkrishna436@gmail.com>
2026-09-08 09:04:04 +0530

    Include media crop images and configure media route for production serving

--- agriconnect_project/urls.py
+++ agriconnect_project/urls.py
@@ -29,6 +29,12 @@ urlpatterns = [
     path('', include('marketplace.urls')),
 ]
 
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT if hasattr(settings, 'STATIC_ROOT') else settings.STATICFILES_DIRS[0])
